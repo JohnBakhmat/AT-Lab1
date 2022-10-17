2dae30c37cb817e214546f367a341db61aecd36f
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-10-17 12:58:53 +0300

    operandIsValid and zero division

--- src/calculator.ts
+++ src/calculator.ts
@@ -6,15 +6,28 @@ const operations = {
 };
 
 export const evaluate = (a: string, b: string, operator: string) => {
-  if (!operatorExists(operator)) {
-    throw Error("Invalid operator");
+  if (!operandIsValid(a) || !operandIsValid(b)) {
+    throw Error("Invalid input");
   }
 
   const parsedA = parseFloat(a);
   const parsedB = parseFloat(b);
+
+  if (parsedB === 0 && operator === "/") {
+    throw Error("Cannot divide by zero");
+  }
+
+  if (!operatorExists(operator)) {
+    throw Error("Invalid operator");
+  }
+
   return operations[operator](parsedA, parsedB);
 };
 
 const operatorExists = (operator: string) => {
   return Object.keys(operations).includes(operator);
+};
+
+const operandIsValid = (operand: string) => {
+  return /^[0-9.-]+$/.test(operand);
 };
